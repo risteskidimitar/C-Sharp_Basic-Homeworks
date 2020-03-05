a94d934ddd002cbba90ca57fac1a69c68651cefb
risteskidimitar <60626102+risteskidimitar@users.noreply.github.com>
2020-03-05 18:25:22 +0100

    commit

---
+++ Homework01CSharp/Homework01CSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Homework01CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("STOP");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("READY");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("GO");
+
+            Console.ReadLine();
+        }
+    }
+}
