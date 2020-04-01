1c0220b2a55cc5795fcc4916f744a2b474be714d
risteskidimitar <60626102+risteskidimitar@users.noreply.github.com>
2020-04-02 00:11:14 +0200

    hw06

---
+++ Homework06CSharp/TASK 01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TASK_01_secondway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
