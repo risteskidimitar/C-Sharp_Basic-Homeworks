d8afe9ee2ef86a8e8fe3d8da4045fe7d33ae2027
risteskidimitar <60626102+risteskidimitar@users.noreply.github.com>
2020-04-08 22:41:31 +0200

    hw

---
+++ Homework08CSharp/Homework08CSharp/Enums/EngineType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework08CSharp.Enums
+{
+    public enum EngineType
+    {
+        petrol = 1,
+        diesel,
+        electric,
+    }
+}
