5b7b1e0b4a4b9f038edb1fb6d54843be4041031f
risteskidimitar <60626102+risteskidimitar@users.noreply.github.com>
2020-04-08 22:35:45 +0200

    hw08

---
+++ Homework08CSharp_draft/Homework08CSharp/Enums/Consumption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework08CSharp.Enums
+{
+    public enum Consumption
+    {
+        economic = 1,
+        medium = 2,
+        high = 3,
+    }
+}
---
+++ Homework08CSharp_draft/Homework08CSharp/Helpers/CodeGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework08CSharp.Helpers
+{
+   internal class CodeGenerator
+    {
+        public static int RandomCodeGnerator()
+        {
+            Random code = new Random();
+            return code.Next(1000, 9999);
+         
+        }
+    }
+}
