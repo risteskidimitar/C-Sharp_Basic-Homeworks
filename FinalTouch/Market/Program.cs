f7c385802c8cd6d7a85cd03f0d7e74f1989a91af
risteskidimitar <60626102+risteskidimitar@users.noreply.github.com>
2020-05-28 12:21:33 +0200

    final

---
+++ FinalTouch/Market/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product();
+
+           // product.Price = 100;
+            Console.WriteLine($"The price of the product is {product.Price}");
+            Console.WriteLine($"The discount of the product is {product.Discount}");
+            int initialValue = 150;
+            product.Price = initialValue;
+
+            //if (product.Price > 100)
+            //    product.Discount = 0.1f;
+            Console.WriteLine($"Initial value is: " + initialValue);
+            Console.WriteLine($"The price of the product is {product.Price}");
+            Console.WriteLine($"The discount of the product is {product.Discount}");
+
+            Console.ReadLine();
+        }
+    }
+}
